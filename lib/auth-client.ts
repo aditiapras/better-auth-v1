40b9dffd40336c7955dd89b43771d295925d0e08
aditiaprasdev <aditiapras97@gmail.com>
2024-11-25 10:25:18 +0700

    remove basePath

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,7 +3,6 @@ import { adminClient } from "better-auth/client/plugins";
 import { nextCookies } from "better-auth/next-js";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL,
   plugins: [adminClient(), nextCookies()],
 });
 
